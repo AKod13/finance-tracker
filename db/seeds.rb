3baf9aef3b3fe31ec0efa0828763ef4deceec9fe
Akod <akodzoman13@gmail.com>
2023-07-06 11:23:32 +0200

    Added views updates and functionalities

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+stocks = Stock.create([{ ticker: 'AAPL', name: 'Apple inc.'  }, { ticker: 'AMZN', name: 'Amazon.com Inc.' }])
